3ceeafc947447246e8372469ef607014965a702f
Abdulsalam <Abdulsalam.axrr44@gmail.com>
2023-07-10 21:57:47 +0300

    - Create Login and Signup pages.

--- app/src/main/java/com/abuadass/mysocialmediaapp/CreatePostActivity.kt
+++ app/src/main/java/com/abuadass/mysocialmediaapp/CreatePostActivity.kt
@@ -8,19 +8,18 @@ import android.provider.MediaStore
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.AppCompatImageView
 import de.hdodenhof.circleimageview.CircleImageView
+class CreatePostActivity : AppCompatActivity(), OnClickListener {
 
-private lateinit var cancelButton: TextView
-private lateinit var addPostButton : TextView
-private lateinit var uploadpostimage : Button
-
-private lateinit var selectedImage: CircleImageView
-
-
-class CreatePostActivity : AppCompatActivity()  , OnClickListener {
+    private lateinit var cancelButton: TextView
+    private lateinit var addPostButton : TextView
+    private lateinit var uploadpostimage : Button
+    private lateinit var selectedImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
@@ -28,16 +27,11 @@ class CreatePostActivity : AppCompatActivity()  , OnClickListener {
         cancelButton = findViewById(R.id.tv_cancelpost)
         addPostButton = findViewById(R.id.tv_add_postbutton)
         uploadpostimage = findViewById(R.id.uploadpostimagebutton)
-
         selectedImage = findViewById(R.id.iv_userpost_image)
-
-
-
 
         cancelButton.setOnClickListener(this)
         addPostButton.setOnClickListener(this)
         uploadpostimage.setOnClickListener(this)
-
         selectedImage.setOnClickListener(this)
     }
 
@@ -45,7 +39,7 @@ class CreatePostActivity : AppCompatActivity()  , OnClickListener {
         if (p0?.id == R.id.tv_cancelpost) {
             openHomePageActivity()
         } else if (p0?.id == R.id.tv_add_postbutton) {
-            openHomePageActivity()
+            submitPost()
         } else if (p0?.id == R.id.uploadpostimagebutton) {
             openUserGallery()
         }
@@ -56,19 +50,25 @@ class CreatePostActivity : AppCompatActivity()  , OnClickListener {
         val pickImg = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         changeImage.launch(pickImg)
     }
-
+    private fun submitPost(){
+        // Upload the image to Firebase Storage.
+        // Get the image URL from the Storage.
+        // Save the image URL to DB.
+        // Save the post on DB.
+    }
     private fun openHomePageActivity() {
         startActivity(Intent(this, HomePageActivity::class.java))
-
     }
     private val changeImage =
         registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
-        ) {
-            if (it.resultCode == Activity.RESULT_OK) {
-                val data = it.data
+        ) { activityResponse->
+            if (activityResponse.resultCode == Activity.RESULT_OK) {
+                val data = activityResponse.data
                 val imgUri = data?.data
                 selectedImage.setImageURI(imgUri)
+            }else{
+                Toast.makeText(this@CreatePostActivity, "You did not pick an image", Toast.LENGTH_LONG).show()
             }
         }
 }
